d816949c2ad2ffaf09189d1da7cb67ffa958e0e2
Ikenna Onugha <i.onugha@alustudent.com>
2026-09-03 08:29:27 +0000

    helloWorld

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    // Print a welcome message to the terminal
+    printf("Hello, C World!\n");
+
+    // Print a basic math operation
+    int a = 5;
+    int b = 10;
+    printf("The sum of %d and %d is %d\n", a, b, a + b);
+
+    return 0;
+}
